70ad48f53ffb4dfa140768cd3cb3faf5a7d7d165
Raquel Nascimento <raquelsol-@hotmail.com>
2020-12-10 20:46:33 -0300

    Estrutura inicial do projeto

---
+++ bidorprojetocore.API/Controllers/CalculadorController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace bidorprojetocore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculadorController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public CalculadorController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("calculajuros")]
+        public IActionResult CalcularJurosComposto(double valorinicial, int meses)
+        {
+            double valorDoJurosMensal = (double)ValorDoJurosMensal();
+
+            if (valorDoJurosMensal == 0)
+            {
+                return NoContent();
+            }
+
+            var juros = 1 + valorDoJurosMensal;
+            var montante = Math.Pow(juros, meses);
+
+            var valorDoJurosComposto = valorinicial * montante;
+            return Ok(valorDoJurosComposto.ToString("N2"));
+        }
+
+        private decimal ValorDoJurosMensal()
+        { 
+            string urlBaseDaAPIDeJuros = _configuration.GetValue<string>("UrlBaseDaAPI:BaseAPIDeJuros");
+
+            HttpClient httpClient = new HttpClient();
+            var juros = httpClient.GetAsync($"{urlBaseDaAPIDeJuros}api/juros/taxaJuros").Result;
+            if (juros.IsSuccessStatusCode)
+            {
+                var valor = juros.Content.ReadAsStringAsync().Result;
+                return Convert.ToDecimal(valor, CultureInfo.GetCultureInfo("en-us"));
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ bidorprojetocore.API/Controllers/JurosController.cs
@@ -0,0 +1,30 @@
+﻿using bidorprojetocore.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bidorprojetocore.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class JurosController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public JurosController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("taxaJuros")]
+        public IActionResult Index()
+        {
+            var taxaPorMes = _configuration.GetValue<string>("TaxaDeJuros:TaxaPorMes");
+            return Ok(Convert.ToDecimal(taxaPorMes, CultureInfo.GetCultureInfo("en-us")));
+        }
+    }
+}
